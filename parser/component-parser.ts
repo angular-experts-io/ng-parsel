2fdaa91ee54aaa80f1aa207d55260d4580e2bf24
kreuzerk <kevin.kreuzer90@icloud.com>
2022-11-23 12:16:03 +0100

    feat: 🎸 (parse) parse component implementation

--- parser/component-parser.ts
+++ parser/component-parser.ts
@@ -5,36 +5,49 @@ import {tsquery} from '@phenomnomnominal/tsquery';
 
 import {MitchellAngularComponent} from "../model/component.model";
 
+// TODO test all of those properties
+export interface MitchellAngularComponentDecorators {
+    selector: string;
+    standalone: boolean;
+    template?: string;
+    templateUrl?: string;
+    styles?: string;
+    styleUrls?: string[];
+    animations?: any[];
+    encapsulation?: string;
+    changeDetection?: string;
+}
+
 export function parseComponent(ast: ts.SourceFile, componentFilePath: string): MitchellAngularComponent {
     const componentDecorators = getAngularComponentDecorators(ast);
     const template =
         componentDecorators.template ? componentDecorators.template :
             fetchFileContent(componentDecorators.templateUrl as string, componentFilePath);
 
-    const styles = componentDecorators.style ?
-        componentDecorators.style :
+    const styles = componentDecorators.styles ?
+        componentDecorators.styles :
         (componentDecorators.styleUrls as [])?.map((styleUrl: string) =>
             fetchFileContent(styleUrl, componentFilePath)
         );
+    const componentImplementation = readFileSync(componentFilePath, 'utf8').toString();
 
-    // TODO: this can probably be improved since we already know the properties and the types
     return {
         name: '',
-        selector: componentDecorators.selector as string,
-        standalone: componentDecorators.standalone as boolean || false,
-        template: template as string,
-        styles: styles as string,
-        implementation: '',
+        selector: componentDecorators.selector,
+        standalone: componentDecorators.standalone || false,
+        template: template,
+        styles: styles,
+        implementation: componentImplementation,
         inputs: [],
         outputs: []
     }
 }
 
-function getAngularComponentDecorators(ast: ts.SourceFile): { [key: string]: string | string[] | boolean } {
+function getAngularComponentDecorators(ast: ts.SourceFile): MitchellAngularComponentDecorators {
     const decoratorQuery = 'Decorator > CallExpression > ObjectLiteralExpression > PropertyAssignment';
     const componentDecorators = tsquery(ast, decoratorQuery);
 
-    return componentDecorators.reduce((decorators: { [key: string]: string | string[] | boolean }, propertyAssignment: any) => {
+    return componentDecorators.reduce((decorators: any, propertyAssignment: any) => {
         const propertyName = propertyAssignment.name.escapedText;
 
         if (propertyName === 'styleUrls' || propertyName === 'styles') {
