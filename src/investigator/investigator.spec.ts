0617f7a5dae5d3d7948f933947d8ff9e68d83215
kreuzerk <kevin.kreuzer90@icloud.com>
2022-11-28 09:07:47 +0100

    test: 💍 (investigator) detect types

--- src/investigator/investigator.spec.ts
+++ src/investigator/investigator.spec.ts
@@ -1,5 +1,64 @@
+import { tsquery } from '@phenomnomnominal/tsquery';
+
+import { investigateType } from './investigator';
+import { NgParselOutputType } from '../parser/shared/model/types.model';
+
 describe('Investigator', () => {
-  it('should be green', () => {
-    expect(true).toBeTruthy();
+  it('should detect Angular components', () => {
+    const ast = tsquery.ast(`
+        @Component({
+            selector: 'app-root',
+        })
+        export class MyTestComponent {}
+      `);
+    expect(investigateType(ast, 'foo.component.ts')).toEqual(NgParselOutputType.COMPONENT);
+  });
+
+  it('should detect Angular modules', () => {
+    const ast = tsquery.ast(`
+        @NgModule({
+            selector: 'app-root',
+        })
+        export class MyTestModule {}
+      `);
+    expect(investigateType(ast, 'foo.module.ts')).toEqual(NgParselOutputType.MODULE);
+  });
+
+  it('should detect Angular pipes', () => {
+    const ast = tsquery.ast(`
+        @Pipe({
+            name: 'app-root',
+        })
+        export class MyTestPipe {}
+      `);
+    expect(investigateType(ast, 'foo.pipe.ts')).toEqual(NgParselOutputType.PIPE);
+  });
+
+  it('should detect Angular directives', () => {
+    const ast = tsquery.ast(`
+        @Directive({
+            selector: 'app-root',
+        })
+        export class MyTestPipe {}
+      `);
+    expect(investigateType(ast, 'foo.directive.ts')).toEqual(NgParselOutputType.DIRECTIVE);
+  });
+
+  it('should detect test files', () => {
+    const ast = tsquery.ast(`
+        describe('foo', () => {
+            it('should do something', () => {
+                expect(true).toBeTruthy();
+            });
+        });
+      `);
+    expect(investigateType(ast, 'foo.spec.ts')).toEqual(NgParselOutputType.SPEC);
+  });
+
+  it('should classify all other files as type UNKNOWN', () => {
+    const ast = tsquery.ast(`
+        const foo = 'bar';
+      `);
+    expect(investigateType(ast, 'foo.ts')).toEqual(NgParselOutputType.UNKNOWN);
   });
 });
